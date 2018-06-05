c65fb9a796bb26ff78ae74f6f356cfe12394bd4a
Alex Vilchis <alexrv99@hotmail.com>
2018-06-05 00:29:53 -0500

    Arregla ApuUrl

--- src/util/ApiUrl.js
+++ src/util/ApiUrl.js
@@ -1,2 +1,2 @@
-// export default "https://control-de-becas-api.herokuapp.com";
-export default "http://localhost:3001";
+export default "https://control-de-becas-api.herokuapp.com";
+// export default "http://localhost:3001";
