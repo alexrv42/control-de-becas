e8a7fb4c66373b740c0f85997a8c028c9730abb0
Alex Vilchis <alexrv99@hotmail.com>
2018-06-06 17:09:07 -0500

    Agrega archivo de acciones comunes en forms

---
+++ src/forms/FormCommons.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import apiUrl from '../util/ApiUrl';
+import 'react-datepicker/dist/react-datepicker.css';
+
+export default class FormCommons {
+
+	static postData(resource, data) {
+		console.log('posting data');
+		fetch(apiUrl + '/' + resource, {
+			method: 'post',
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(data)
+		}).then(res => res.json())
+		.then(res => console.log(res));
+	}
+
+	static submitAnimation()
+	{
+		window.scrollTo(0, 0);
+	}
+
+}
